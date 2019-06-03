ccdbc4a976dce1caf38cced094ce6708fec3d351
Jeff Tian <jie.tian@unicareer.com>
2019-06-03 15:14:55 +0800

    fix: test

--- test/grpc-client.test.ts
+++ test/grpc-client.test.ts
@@ -1,18 +1,49 @@
-"use strict"
 import {Metadata} from 'grpc'
+import {spawn} from 'child_process'
 
 const mock = require("egg-mock")
 
 describe("test/grpc-client.test.js", () => {
     let app: any
+    let grpcServer: any
     before(async () => {
+        grpcServer = spawn('ts-node', ['test/fixtures/apps/grpc-client-test/grpc/grpc-server.ts'], {
+            detached: true,
+        })
+
+        grpcServer.on('close', (code: any) => {
+            console.log(`child process exited with code ${code}`)
+        })
+
+        console.log('grpc server started...', grpcServer.killed)
+
+        await new Promise((resolve, reject) => {
+            let i = 0
+            grpcServer.stdout.on('data', (data: any) => {
+                console.log(`received data ${i++}:`, data.toString())
+                resolve(data)
+            })
+
+            grpcServer.stderr.on('data', (data: any) => {
+                console.error(`received error: `, data.toString())
+                reject(data)
+            })
+        })
+
         app = mock.app({
             baseDir: "apps/grpc-client-test"
         })
         return await app.ready()
     })
 
-    after(() => app.close())
+    after(() => {
+        app.close()
+        console.log('grpcServer is ', grpcServer.killed)
+        if (!grpcServer.killed) {
+            grpcServer.kill()
+        }
+    })
+
     afterEach(mock.restore)
 
     it("should GET /", async () => {
